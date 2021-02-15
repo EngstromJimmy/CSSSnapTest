fe016fec7fd0083ea4a88051e756f6feecf831a4
Jimmy Engström <jimmy@apeoholic.se>
2021-02-15 22:30:50 +0100

    Added sample

---
+++ Pages/Segment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp13.Pages
+{
+    public class Segment
+    {
+        public string MyProperty { get; set; }
+    }
+}
